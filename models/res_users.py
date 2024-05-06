8265d01c2560132cc3b6e063fbab35f0da5c163b
Dnjomou Loic <dnjomouloic@gmail.com>
2024-05-06 08:55:00 +0100

    remove examinateur

--- models/res_users.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResUsersInherit(models.Model):
-    _name = 'res.users'
-    _inherit = 'res.users'
-
-    is_examiner = fields.Boolean(string="Est Un Examinateur ?", default=False)
